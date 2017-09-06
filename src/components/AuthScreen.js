d8376206c99fa9b54b5bf63082b6b535174a9363
jacques mouette <j.mouette@gmail.com>
2017-09-05 21:14:38 -0300

    WIP: GitHub OAuth

--- src/components/AuthScreen.js
+++ src/components/AuthScreen.js
@@ -5,14 +5,48 @@ import {
   View,
   Button
 } from 'react-native';
+import OAuthManager from 'react-native-oauth';
+
 
 export default class AuthScreen extends Component {
+  componentWillMount() {
+    const config = {
+      github: {
+        callback_url: `http://localhost/github`,
+        client_id: 'd38f51462899fbf69edb',
+        client_secret: 'e1959389be6dba164092011d28353db1afc4c8b7'
+      }
+    }
+
+    this.manager = new OAuthManager('gistreader')
+
+    this.manager.configure(config);
+    // console.log(this.manager);
+  }
+
+  login() {
+    console.log('login');
+    this.manager.authorize('github', {scopes: 'user:email gist'})
+      .then((res) => {
+        console.log('success');
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Auth Screen!
         </Text>
+        <Text style={styles.welcome}>
+          
+        </Text>
+        <Button
+          title='Login with Github'
+          onPress={() => this.login()}
+        />
         <Button 
           title='To Reader >'
           onPress={() => this.props.navigation.navigate('Reader')}
