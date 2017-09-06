36e240a95d67687b14c1f8fefc2bee3a3bc822d7
jacques mouette <j.mouette@gmail.com>
2017-09-06 14:19:31 -0300

    Fix missing comma

--- android/app/src/main/java/com/gistreader/MainApplication.java
+++ android/app/src/main/java/com/gistreader/MainApplication.java
@@ -24,9 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new RCTCameraPackage()
-          new OAuthManagerPackage()
+        new MainReactPackage(),
+        new RCTCameraPackage(),
+        new OAuthManagerPackage()
       );
     }
   };
